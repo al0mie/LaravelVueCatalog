a8e8de266bf0cbc7bb5a46bc27bbd76697e8d6b8
al0mie <alex.mokrencko@gmail.com>
2017-03-07 20:21:09 +0300

    stubs in controllers

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($id)
+    {
+
+    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+        
+    }
+    
+    public function store()
+    {
+        
+    }
+    
+    public function edit($id)
+    {
+        
+    }
+    
+    public function update($id)
+    {
+        
+    }
 }
