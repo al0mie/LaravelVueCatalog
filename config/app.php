0e52f6ce9a13a04ebc2b06bb7e553af3ae5e1204
al0mie <alex.mokrencko@gmail.com>
2017-03-07 19:55:41 +0300

    set baum for nested set models

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Custom Service Providers...
+        */
+        'Baum\Providers\BaumServiceProvider'
     ],
 
     /*
