7175c57ac572f131367e32c6816736a58249cc5c
al0mie <alex.mokrencko@gmail.com>
2017-03-07 21:14:16 +0300

    crud for categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,27 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use App\Services\CategoryService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CategoryController extends Controller
 {
-    public function __construct()
-    {
+    private $categoryService;
 
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
     }
 
     public function index()
     {
+        $categories = Category::orderBy('id');
 
+        return $categories->paginate(50);
     }
 
-    public function store()
+    public function store(CategoryRequest $request)
     {
-
+        $category = new Category();
+        $this->categoryService->saveCategory($request, $category);
+        return response($category, Response::HTTP_CREATED);
     }
 
-    public function update($id)
+    public function show($id)
     {
+        return Category::findOrFail($id);
+    }
 
+    public function update(CategoryRequest $request, $id)
+    {
+        $category = Category::findOrFail($id);
+        $this->categoryService->saveCategory($request, $category);
+        return response($category, Response::HTTP_CREATED);
+    }
+
+    public function destroy($id)
+    {
+        Category::destroy($id);
     }
 }
--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -4,5 +4,13 @@ namespace App\Services;
 
 class CategoryService
 {
-
+    /**
+     * @param $request
+     * @param $product
+     */
+    public function saveCategory($request, $category)
+    {
+        $category->name = $request->name;
+        $category->save();
+    }
 }
